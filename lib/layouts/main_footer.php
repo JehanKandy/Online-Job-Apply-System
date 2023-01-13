0544d43cac874c374581d6cf05890d1e5c695356
JehanKandy <jehankandy@gmail.com>
2023-01-13 18:32:09 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -5,9 +5,9 @@
         <div class="container">
             <div class="footer-grid">
                 <div class="footer-item1">Footer 1</div>
-                <div class="footer-item1">Footer 2</div>
-                <div class="footer-item1">Footer 3</div>
-                <div class="footer-item1">Footer 4</div>
+                <div class="footer-item2">Footer 2</div>
+                <div class="footer-item3">Footer 3</div>
+                <div class="footer-item4">Footer 4</div>
             </div>
         </div>
     </div>
