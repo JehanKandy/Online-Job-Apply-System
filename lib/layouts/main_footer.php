ccd536dc1fb6bc8a452aa2fd00f5a140592f3a41
JehanKandy <jehankandy@gmail.com>
2023-01-13 19:13:50 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -1,102 +1,43 @@
-@import url('https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300&family=Overpass&family=PT+Sans+Narrow&family=Source+Sans+Pro&display=swap');
+<link rel="stylesheet" href="../../css/style.css">
 
-/*
-    font-family: 'Merriweather Sans', sans-serif;
-    font-family: 'Overpass', sans-serif;
-    font-family: 'PT Sans Narrow', sans-serif;
-    font-family: 'Source Sans Pro', sans-serif;
-*/
+<footer>
+    <div class="footer-content">
+        <div class="container">
+            <div class="footer-grid">
+                <div class="footer-item1">
+                    <div class="title">ABC Jobs</div>
+                    <div class="body">
+                        The ABC Job System is Develop for educational purposes only 
+                    </div>
+                </div>
+                <div class="footer-item2">
+                    <div class="title">Useful Links</div>                
+                    <div class="body">
+                        <ul>
+                            <li><a href="#">Useful Link 1</a></li>
+                            <li><a href="#">Useful Link 2</a></li>
+                            <li><a href="#">Useful Link 3</a></li>
+                            <li><a href="#">Useful Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-item3">
+                    <div class="title">Our Services</div>
+                    <div class="body">
+                        <ul>
+                            <li><a href="#">Service 1</a></li>
+                            <li><a href="#">Service 2</a></li>
+                            <li><a href="#">Service 3</a></li>
+                            <li><a href="#">Service 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-item4">
+                    <div class="title">Contact Us</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
-.footer-content{
-    padding: 80px 0 80px 0;
-    font-family: 'Overpass', sans-serif;
-    background: -webkit-linear-gradient(rgba(14, 13, 13, 0.815), rgba(31, 31, 31, 0.815)), url(../images/footer.jpg);
-    color: white;
-    background-position: center;
-    background-size: cover;
-}
-.footer-content .footer-grid{
-    display: grid;
-    width: 100%;
-    height: auto;
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: minmax(250px, auto);
-    grid-gap: 1rem;
-    grid-template-areas: "a b c d";
-    padding: 45 45 45 45;
-    margin-top: 40px;
-    margin-bottom: 20px;
-}
-.footer-item1{
-    grid-area: a;
-}
-.footer-item2{
-    grid-area: b;
-}
-.footer-item3{
-    grid-area: c;
-}
-.footer-item4{
-    grid-area: d;
-}
-.footer-item1  .title, .footer-item2  .title, .footer-item3  .title, .footer-item4 .title{
-    text-align: center;
-    font-size: 30px;
-}
-.footer-item1 .body, .footer-item2 .body, .footer-item3 .body, .footer-item4 .body{
-    margin-top: 20px;
-}
-.footer-item1 .footer-logo{
-    width: 150px;
-    height: 120px;
-    border: 5px solid white;
-}
-.footer-item2 ul li, .footer-item3 ul li{
-    list-style-type: none;
-    font-size: 18px;
-    margin: 10px 0 10px 0;
-}
-.footer-item2, .footer-item3, ul li a{
-    color: white;
-}
-/* 
-    ***************************************************************************************
-
-                             commen laptop screen and small screens
-
-                                            769px - 1400px
-
-    ***************************************************************************************
-*/
-
-@media only screen and (min-device-width: 821px) and (max-device-width: 1400px) {
-
-}
-
-/* 
-    ***************************************************************************************
-
-                                        iPads, Tablets
-
-                                        481px — 768px
-
-    ***************************************************************************************
-*/
-
-@media only screen and (min-device-width: 481px) and (max-device-width: 820px) {
-
-}
-
-/* 
-    ***************************************************************************************
-
-                                     commen mobile screen
-
-                                            320x480
-
-    ***************************************************************************************
-*/
-
-@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
-
-}
+<script src="../../js/script.js"></script>
