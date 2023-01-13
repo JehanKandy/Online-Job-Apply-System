c4373905557dda3fd647cfb023dd06da7149f3f0
JehanKandy <jehankandy@gmail.com>
2023-01-13 18:08:29 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -3,7 +3,12 @@
 <footer>
     <div class="footer-content">
         <div class="container">
-            hi
+            <div class="footer-gird">
+                <div class="footer-item1">Footer 1</div>
+                <div class="footer-item1">Footer 2</div>
+                <div class="footer-item1">Footer 3</div>
+                <div class="footer-item1">Footer 4</div>
+            </div>
         </div>
     </div>
 </footer>
