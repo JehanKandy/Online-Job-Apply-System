19b650b620464333d291150ffc98a203c6177cab
JehanKandy <jehankandy@gmail.com>
2023-01-14 13:23:52 +0530

    Update footer_page.php

---
+++ lib/layouts/footer_page.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../../css/style.css">
+<footer>
+    <div class="footer-page">
+        <div class="container">
+        All rights reserved 2021 - 2023 <b><i class="far fa-copyright"></i>JK Jobs</b> &nbsp; Develop By <a href="https://github.com/JehanKandy" target="_blank">JehanKandy</a>
+        </div>
+    </div>
+</footer>
+
+<script src="../../js/script.js"></script>
