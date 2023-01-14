d0f163990c7267208576b8318ad2bc05b1ac0c9c
JehanKandy <jehankandy@gmail.com>
2023-01-14 20:54:34 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -14,14 +14,32 @@
         $select_user_nor = mysqli_num_rows($select_user_result);
 
         if($select_user_nor > 0){
-            return  "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                     <strong>User Error</strong>User already exists..!
                     <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                     <span aria-hidden='true'>&times;</span>
                     </button>
             </div>";
         }else{
+            $insert_data = "INSERT INTO user_tbl(username,email,pass_user,is_active,is_pending,join_date)VALUES('$username','$email','$pass',0,1,NOW())";
+            $insert_data_result = mysqli_query($con, $insert_data);
 
+            if(!$insert_data_result){
+                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>User Error</strong>User already exists..!
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                </div>";
+            }
+            else{
+                return  "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                        <strong>Successfully</strong>Account Created Successfully <a href='login.php'>Login Here</a>
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                </div>";
+            }
         }
     }    
 
