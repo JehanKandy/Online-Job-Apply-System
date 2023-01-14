1d82bf5ce6746d8de62e8abcdedd74da4034b5bd
JehanKandy <jehankandy@gmail.com>
2023-01-14 13:14:54 +0530

    Add files via upload

---
+++ lib/views/login.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../../css/style.css">
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/login_nav.php"); ?>
+
+
+
+
+<script src="../../js/script.js"></script>
