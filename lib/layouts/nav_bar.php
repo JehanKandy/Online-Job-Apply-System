ebfe773a05f1c82ef78cc75f4d0bcfc22eb7c3c9
JehanKandy <jehankandy@gmail.com>
2023-01-14 08:59:44 +0530

    Update nav_bar.php

--- lib/layouts/nav_bar.php
+++ lib/layouts/nav_bar.php
@@ -1,3 +1,12 @@
 <link rel="stylesheet" href="../../css/style.css">
+<nav>
+    <div class="nav-content">
+        <div class="container">
+            <div class="title">ABC Jobs</div>
+            <div class="login-nav">Hi</div>
+        </div>
+    </div>
+</nav>
+
 
 <script src="../../js/script.js"></script>
