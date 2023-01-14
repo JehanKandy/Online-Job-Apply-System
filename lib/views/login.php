66dafe0b07de82f9111fb723923d9049988630d1
JehanKandy <jehankandy@gmail.com>
2023-01-14 13:42:16 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -3,6 +3,27 @@
 <?php include("../layouts/login_nav.php"); ?>
 
 
+<div class="container">
+    <div class="card login-card">
+    <div class="card-header">
+        <i class="fas fa-user-alt"></i> &nbsp; Login
+    </div>
+    <div class="card-body login-card-body">
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+            <p class="form-text" id="username">Username : </p>
+            <input type="text" name="userName" id="usern" class="form-control form-input">
+            <p class="usernError"></p>
 
+            <p class="form-text" id="password">Password : </p>
+            <input type="password" name="passWord" id="passn" class="form-control form-input">
 
+            <input type="submit" value="Login" class="btn btn-primary login-btn-form" name="login">
+        </form>
+        <p>Don't have an Account ? <a href="reg.php" style="color: rgb(123, 8, 231);">Sign Up</a></p>
+        <p>Forget Password ? <a href="forget_pass.php" style="color:rgb(123, 8, 231);">Forget Password</a></p>
+    </div>
+    </div>
+</div>
+
+<?php include("../layouts/footer_page.php"); ?>
 <script src="../../js/script.js"></script>
