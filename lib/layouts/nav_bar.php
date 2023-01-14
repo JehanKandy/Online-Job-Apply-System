adc257bbee4f9112b2a4c69306088dbfd53c62c8
JehanKandy <jehankandy@gmail.com>
2023-01-14 13:03:17 +0530

    Update nav_bar.php

--- lib/layouts/nav_bar.php
+++ lib/layouts/nav_bar.php
@@ -5,8 +5,8 @@
         <div class="container">
             <div class="title">ABC Jobs</div>
             <div class="login-nav">
-                <a href="#"><button class="login-btn"><i class="fas fa-user-alt"></i> Login</button></a>
-                <a href="#"><button class="login-btn"><i class="fas fa-user-plus"></i> Sign Up</button></a>
+                <a href="../views/login.php"><button class="login-btn"><i class="fas fa-user-alt"></i> Login</button></a>
+                <a href="../views/reg.php"><button class="login-btn"><i class="fas fa-user-plus"></i> Sign Up</button></a>
             </div>
         </div>
     </div>
