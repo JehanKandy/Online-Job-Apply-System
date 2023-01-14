7fb7d91ce7173be216bc43f9b442d8162906a098
JehanKandy <jehankandy@gmail.com>
2023-01-14 20:40:51 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -7,6 +7,11 @@
 
     function reguser($username, $email, $pass){
         $con = Connection();
+
+        $select_user = "SELECT * FROM user_tbl WHERE email = '$email' && username = '$username'";
+        $select_user_result = mysqli_query($con, $select_user);
+        $select_user_row = mysqli_fetch_assoc($select_user_result);
+        $select_user_nor = mysqli_num_rows($select_user_result);
     }    
 
 ?>
