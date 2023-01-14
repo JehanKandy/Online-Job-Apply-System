7761ab2928e61a8d4d2d2971438dfdfbbe6dd2e4
JehanKandy <jehankandy@gmail.com>
2023-01-14 13:15:25 +0530

    Add files via upload

---
+++ lib/layouts/login_nav.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../../css/style.css">
+
+<nav>
+    <div class="nav-content">
+        <div class="container">
+            <div class="title">ABC Jobs</div>
+        </div>
+    </div>
+</nav>
+
+<script src="../../js/script.js"></script>
