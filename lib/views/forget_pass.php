2766fd5e5f1e18728e245dc7750ad9baf15a445e
JehanKandy <jehankandy@gmail.com>
2023-01-15 08:47:03 +0530

    Update forget_pass.php

--- lib/views/forget_pass.php
+++ lib/views/forget_pass.php
@@ -4,11 +4,11 @@
 
 <div class="forget-pass">
     <div class="container">
-        <div class="card">
+        <div class="card login-card">
             <div class="card-header">
                 Featured
             </div>
-            <div class="card-body">
+            <div class="card-body login-card-body">
                 <h5 class="card-title">Special title treatment</h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
