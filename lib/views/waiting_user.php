f69305d0d76ad5418636be83a0690d3ce148119f
JehanKandy <jehankandy@gmail.com>
2023-01-15 07:29:39 +0530

    Update waiting_user.php

--- lib/views/waiting_user.php
+++ lib/views/waiting_user.php
@@ -2,5 +2,11 @@
 <link rel="stylesheet" href="../../css/style.css">
 <?php include("../layouts/login_nav.php"); ?>
 
+<div class="waiting-content">
+    <div class="container">
+        
+    </div>
+</div>
+
 <?php include("../layouts/footer_page.php"); ?>
 <script src="../../js/script.js"></script>
