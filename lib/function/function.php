32630810dd01a8b6961ebb12f572a004e4c80e67
JehanKandy <jehankandy@gmail.com>
2023-01-15 10:30:21 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -118,5 +118,14 @@
 
     function forget_pass($username,$email){
         $con = Connection();
+
+        $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email' && is_active = 1 && is_pending = 0";
+        $check_user_result = mysqli_query($con, $check_user);
+        $check_user_row = mysqli_fetch_assoc($check_user_result);
+        $check_user_nor = mysqli_num_rows($check_user_result);
+
+        if($check_user_nor > 0){
+
+        }
     }
 ?>
