f3b2072d535e592f0651c5e9a61c123946d3ec7f
JehanKandy <jehankandy@gmail.com>
2023-01-15 07:32:24 +0530

    Update waiting_user.php

--- lib/views/waiting_user.php
+++ lib/views/waiting_user.php
@@ -4,7 +4,16 @@
 
 <div class="waiting-content">
     <div class="container">
-        hi
+        <div class="card">
+            <div class="card-header">
+                Waiting User
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     </div>
 </div>
 
