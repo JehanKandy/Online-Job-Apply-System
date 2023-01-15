3b8273b033f1e3d076985c7b67840f73f3111ed8
JehanKandy <jehankandy@gmail.com>
2023-01-15 07:36:18 +0530

    Update waiting_user.php

--- lib/views/waiting_user.php
+++ lib/views/waiting_user.php
@@ -9,9 +9,7 @@
                 <i class="fas fa-user-clock"></i> Waiting User
             </div>
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                
             </div>
         </div>
     </div>
