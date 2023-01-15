c39a68ae120bbb91ded89ebeabc1b13df2a5206f
JehanKandy <jehankandy@gmail.com>
2023-01-15 11:27:21 +0530

    Update verify_otp.php

--- lib/views/verify_otp.php
+++ lib/views/verify_otp.php
@@ -1,5 +1,11 @@
 <link rel="stylesheet" href="../../css/style.css">
 <?php include("../layouts/login_nav.php"); ?>
 
+<div class="verify-otp">
+    <div class="container">
+        
+    </div>
+</div>
+
 <?php include("../layouts/footer_page.php"); ?>
 <script src="../../js/script.js"></script>
