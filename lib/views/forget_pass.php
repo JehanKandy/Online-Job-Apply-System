478d96b1219295a09e67b7924659fe0bcbdac224
JehanKandy <jehankandy@gmail.com>
2023-01-15 08:15:19 +0530

    Update forget_pass.php

--- lib/views/forget_pass.php
+++ lib/views/forget_pass.php
@@ -1,5 +1,8 @@
+<?php include("../layouts/header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 <?php include("../layouts/login_nav.php"); ?>
+
+
 
 <?php include("../layouts/footer_page.php"); ?>
 <script src="../../js/script.js"></script>
