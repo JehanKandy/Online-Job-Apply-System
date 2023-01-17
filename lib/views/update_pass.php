769e9f5c4a6f55db7c4b9bc281fca3363baf2c1d
JehanKandy <jehankandy@gmail.com>
2023-01-17 08:06:29 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -10,9 +10,7 @@
                 <i class="fas fa-key"></i> Update Password
             </div>
             <div class="card-body login-card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+
             </div>
         </div>
     </div>
