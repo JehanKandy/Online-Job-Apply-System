db7d7550c087b9139d32f9846215ebd2fc29a977
JehanKandy <jehankandy@gmail.com>
2023-01-17 08:59:47 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -246,6 +246,7 @@
                                 <span aria-hidden='true'>&times;</span>
                                 </button>
                         </div>";
+                        
                     }elseif($update_user_tbl_result){
                         header("location:logout.php");
                     }
