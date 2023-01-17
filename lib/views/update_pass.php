77a2ce93fb9b8a4afb5ff357e645adf14631135c
JehanKandy <jehankandy@gmail.com>
2023-01-17 08:02:14 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -2,3 +2,7 @@
 <link rel="stylesheet" href="../../css/style.css">
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/login_nav.php"); ?>
+
+
+<?php include("../layouts/footer_page.php"); ?>
+<script src="../../js/script.js"></script>
