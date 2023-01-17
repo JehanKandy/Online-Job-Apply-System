06e0cc4c74127e4a0727cdb8024ae2358f1a06ed
JehanKandy <jehankandy@gmail.com>
2023-01-17 08:05:04 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -3,6 +3,20 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/login_nav.php"); ?>
 
+<div class="update-pass-content">
+    <div class="container">
+        <div class="card login-card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include("../layouts/footer_page.php"); ?>
 <script src="../../js/script.js"></script>
