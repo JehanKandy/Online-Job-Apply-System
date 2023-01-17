b51c76a93c4409449420ef88452373306b8a1a52
JehanKandy <jehankandy@gmail.com>
2023-01-17 07:59:20 +0530

    Add files via upload

---
+++ lib/views/update_pass.php
@@ -0,0 +1,4 @@
+<?php include("../function/function.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/login_nav.php"); ?>
