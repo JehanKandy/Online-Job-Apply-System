4097e867e6dfde9b75dc63ca2a5135656a3d9175
JehanKandy <jehankandy@gmail.com>
2023-01-17 08:05:23 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -9,7 +9,7 @@
             <div class="card-header">
                 Featured
             </div>
-            <div class="card-body">
+            <div class="card-body login-card-body">
                 <h5 class="card-title">Special title treatment</h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
